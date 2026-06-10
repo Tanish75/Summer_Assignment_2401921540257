db315ee35c859b7f521fd0d52476079cdd43cde8
Tanish75 <tanish.gusknp2024@gmail.com>
2026-06-10 22:32:43 +0530

    Create IsSubsequence.cxx

---
+++ Week_2/Day_3/IsSubsequence.cxx
@@ -0,0 +1,36 @@
+/*
+  A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:*/
+
+
+
+
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.size();
+    }
+};
