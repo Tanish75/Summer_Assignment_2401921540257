b7b4cd7dbd16718e56929b67259b8388d5d6a978
Tanish75 <tanish.gusknp2024@gmail.com>
2026-06-17 23:14:02 +0530

    Create MinStack.cxx

---
+++ Week_3/Day_3/MinStack.cxx
@@ -0,0 +1,69 @@
+/*
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int value) pushes the element value onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ */
+
+#include <stack>
+
+class MinStack {
+private:
+    std::stack<int> mainStack;
+    std::stack<int> minStack;
+
+public:
+    MinStack() {}
+    
+    void push(int val) {
+        mainStack.push(val);
+        
+        // Only push to minStack if it's the new minimum
+        if (minStack.empty() || val <= minStack.top()) {
+            minStack.push(val);
+        }
+    }
+    
+    void pop() {
+        // If we pop the current minimum, remove it from minStack too
+        if (mainStack.top() == minStack.top()) {
+            minStack.pop();
+        }
+        mainStack.pop();
+    }
+    
+    int top() {
+        return mainStack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
